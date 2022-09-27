e32cc966095d43c50995e221ccea698e28905d4f
Joairazabal <95320625+Joairazabal@users.noreply.github.com>
2022-09-27 12:00:13 -0300

    detail responsive mdd

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import {createRoot} from "react-dom/client";
+import {Provider} from "react-redux";
 import store from "./redux/store";
 import App from "./App";
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import {Auth0Provider} from '@auth0/auth0-react'
+import {BrowserRouter} from 'react-router-dom';
+
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-const clientId= process.env.REACT_APP_AUTH0_CLIENT_ID
-const dominio= process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+const dominio = process.env.REACT_APP_AUTH0_DOMAIN
 
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-    <Auth0Provider domain={dominio} clientId={clientId} redirectUri={window.location.origin}>
-    <App/>
-    </Auth0Provider>
-    </BrowserRouter>
-  </Provider>
+root.render (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 );
